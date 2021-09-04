02568145d10c81572229161660a8d3efa997cb84
EC2 Default User <ec2-user@ip-172-31-16-231.ec2.internal>
2021-09-04 15:46:29 +0000

    応用課題7c

---
+++ app/models/group.rb
@@ -0,0 +1,5 @@
+class Group < ApplicationRecord
+  attachment :image
+  has_many :group_users
+  has_many :users, through: :group_users
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'searches/search'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -17,4 +18,7 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+
+  resources :groups
+  
 end
